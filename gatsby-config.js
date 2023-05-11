3eeb8410d382168b896f5107a564e533c3147e7c
Eric Phifer <ericoncode@gmail.com>
2023-05-11 11:01:26 -0600

    add in all needed dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,32 @@ module.exports = {
         dataset: 'production',
       },
     },
+    {
+      resolve: 'gatsby-plugin-sanity-image',
+      options: {
+        projectId: 'wzk0i0ir',
+        dataset: 'production',
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-simple-analytics',
+      options: {
+        trackPageViews: true,
+        events: true,
+        eventsGlobal: 'sa_event',
+        // ignorePages: ["pathname"],
+      },
+    },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://kinnashouseoflove.com',
+        sitemap: 'https://kinnashouseoflove.com/sitemap/sitemap-index.xml',
+        policy: [{ userAgent: '*', allow: '/' }],
+      },
+    },
+    'gatsby-plugin-netlify',
+    'gatsby-plugin-react-helmet',
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
